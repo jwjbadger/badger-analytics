c8c6c64b5e5a5f91ac5ceb277b889dc6e2500a9b
Joseph Johnson <jwjbadger@gmail.com>
2021-05-31 17:10:56 -0700

    revamp generate function

--- badger_analytics/regression/linear.py
+++ badger_analytics/regression/linear.py
@@ -3,10 +3,4 @@ import numpy as np
 
 # generates theta matrix 
 def generate(n):
-    result = np.array([])
-    
-    if n < 1:
-        return result
-    
-    for i in range(1,n):
-        np.append(result, random.random() * 10)
+    return np.multiply(np.random.rand(n), 10)
