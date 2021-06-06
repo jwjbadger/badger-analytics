91028a4ec5d2235217fee8a52632e34e49c8b214
jwjbadger <jwjbadger@gmail.com>
2021-06-06 16:49:09 -0500

    add gradient descent
    for univariate linear regression (tested)

--- badger_analytics/regression/linear.py
+++ badger_analytics/regression/linear.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # generates theta matrix 
 def generate(n):
-    return np.multiply(np.random.rand(n), 10)
+    return np.multiply(np.random.rand(n, 1), 10)
 
 def squared_error(x, y, theta):
     m = x.shape[0] # number of rows in x
@@ -10,3 +10,17 @@ def squared_error(x, y, theta):
     h = np.dot(X, theta) # h(x)
 
     return (1/(2*m)) * np.sum(np.power((np.subtract(h, y)), 2))
+
+def gradient_descent(i, alpha, x, y, init_theta): # squared error only currently
+    theta = init_theta # set a mutatable theta
+    m = x.shape[0] # number of rows in x
+    X = np.hstack((np.ones((m,1)),x)) # x with a prepended column of ones
+
+    for i in range(1, i):
+        h = np.dot(X, theta) # h(x)
+
+        temp0 = theta[0][0] - (alpha * (1/m) * np.sum(np.subtract(h, y)))
+        temp1 = theta[1][0] - (alpha * (1/m) * np.sum(np.multiply(np.subtract(h, y), x)))
+        theta = np.array([[temp0], [temp1]])
+
+    return theta
