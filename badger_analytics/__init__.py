0f06412f65d53ea91775735dc97df4b48da81ff6
jwjbadger <jwjbadger@gmail.com>
2021-06-06 15:46:28 -0500

    add squared error cost function
    for univariate linear regression (tested
    )

--- badger_analytics/__init__.py
+++ badger_analytics/__init__.py
@@ -1,2 +1,4 @@
 from . import data_import
 from . import main
+from . import processing
+from . import regression
--- badger_analytics/regression/linear.py
+++ badger_analytics/regression/linear.py
@@ -1,6 +1,12 @@
-import random
 import numpy as np
 
 # generates theta matrix 
 def generate(n):
     return np.multiply(np.random.rand(n), 10)
+
+def squared_error(x, y, theta):
+    m = x.shape[0] # number of rows in x
+    X = np.hstack((np.ones((m,1)),x)) # x with a prepended column of ones
+    h = np.dot(X, theta) # h(x)
+
+    return (1/(2*m)) * np.sum(np.power((np.subtract(h, y)), 2))
