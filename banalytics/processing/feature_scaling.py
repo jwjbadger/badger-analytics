ab07d9de4a1af4e783ce88cc08a5073d3925e9e9
jwjbadger <jwjbadger@gmail.com>
2021-06-08 16:50:29 -0500

    add documentation to feature_scaling

--- banalytics/processing/feature_scaling.py
+++ banalytics/processing/feature_scaling.py
@@ -1,15 +1,47 @@
 import numpy as np
 
 def standardization(input_set):
+    """
+    standardization(input_set)
+
+    Standardizes the data stored within `input_set`.
+
+    Parameters
+    ----------
+    input_set : numpy.array
+        a numpy array to standardize
+
+    Returns
+    -------
+    output : numpy.array
+        a numpy array with the standardized values of the original array
+    """
+
     result = np.array([])
-    X = np.hsplit(input_set, len(input_set[0]))
+    X = np.hsplit(input_set, len(input_set[0])) # split the input array into columns
     for i in X:
         result = np.append(result, ((i - np.mean(i))/np.std(i)))
     return result
 
 def normalization(input_set):
+    """
+    normalization(input_set)
+
+    Normalizes the data stored within `input_set`.
+
+    Parameters
+    ----------
+    input_set : numpy.array
+        a numpy array to normalize
+
+    Returns
+    -------
+    output : numpy.array
+        a numpy array with the normalized values of the original array
+    """
+
     result = np.array([])
-    X = np.hsplit(input_set, len(input_set[0]))
+    X = np.hsplit(input_set, len(input_set[0])) # split the input array into columns
     for i in X:
         result = np.append(result, (i-min(i))/(max(i)-min(i)))
     return result
