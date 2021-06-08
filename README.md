e5c82444898a7f14eb294b296f025d403af7249e
jwjbadger <jwjbadger@gmail.com>
2021-06-08 16:07:13 -0500

    add documentation to regression

--- README.md
+++ README.md
@@ -1,47 +1,11 @@
 # Badger Analytics
 
-## Introduction
-
 This is a package that is built to do basic data analysis from scratch. This package is meant to give a large amount of customizability in each data analysis method.
 
-## Importing Data
+## Installing
 
-There are two main methods of importing external data.
+Install with `pip install banalytics` or for user install `pip install -u banalytics`
 
-### Local
+## Getting Started
 
-If you want to import a file that is on your machine, you can use the function `data_import.local_import(path)`
 
-This function takes a path as an input. This can be a relative or absolute path. Data should be formatted using standard python array format (i.e. `[[1,2,3],[4,5,6],[7,8,9]]`)
-
-This function returns a [NumPy](https://NumPy.org/ "NumPy home page") array.
-
-### Remote
-
-If you want to import a file via HTTP get request, you can use the `data_import.remote_import(path)` function
-
-This function is exactly the same as `data_import.local_import()` except the path it takes as input should be a website. Be sure to include "http://" or "https://" in front of the path.
-
-## Data Processing
-
-Before any analytical proceses can be performed, the data may have to be cleaned up.
-
-## Feature Scaling
-
-The goal of feature scaling is to compress data so that certain data analysis algorythms will work better.
-
-### Standardization
-
-This method of feature scaling is accessible via `processing.feature_scaling.standardization(input_set)`
-
-It returns the z score of each value in a list or matrix. The z score represents the distance in standard deviations from the mean.
-
-Given a 2D NumPy array with more than 1 row, this function will return the z scores of the inputed data set's numerical values. The result will be a NumPy array.
-
-### Normalization
-
-Normalization is a type of feature scaling that is accessible via `processing.feature_scaling.normalization(input_set)`
-
-It returns the value of the number scaled down between 0 and 1 in respect to the minimum and maximum.
-
-Given a 2D NumPy array with more than 1 row, this function will return the scaled down version of the inputed data set's numerical values. The result will be a NumPy array.
--- banalytics/regression/linear.py
+++ banalytics/regression/linear.py
@@ -1,18 +1,76 @@
 import numpy as np
 
-# generates theta matrix 
+# generates theta vector 
 def generate(n):
+    r"""
+    generate(n)
+
+    Generates a random theta vector of size n with a uniform distribution over [0, 10).
+
+    Parameters
+    ----------
+    n : int
+        an integer that describes the size of the resulting theta vector
+
+    Returns
+    -------
+    output : numpy.array
+        a 2d numpy array of the randomly generated theta matrix
+    """
+
     return np.multiply(np.random.rand(n, 1), 10)
 
 def squared_error(x, y, theta):
+    r"""
+    squared_error(x, y, theta)
+
+    Gives the squared error for given x and y vectors and a theta vector.
+
+    Parameters
+    ----------
+    x : numpy.array
+        a 2d numpy array that holds the x values as a vector
+    y : numpy.array
+        a 2d numpy array that holds the y values as a vector
+    theta : numpy.array
+        a 2d numpy array that holds the theta values as a vector
+
+    Returns
+    -------
+    output : float
+        a float of the squared error with the given parameters
+    """
+
     m = x.shape[0] # number of rows in x
     X = np.hstack((np.ones((m,1)),x)) # x with a prepended column of ones
     h = np.dot(X, theta) # h(x)
 
     return np.round((1/(2*m)) * np.sum(np.power((np.subtract(h, y)), 2)), 6)
 
-def gradient_descent(i, alpha, x, y, init_theta): # squared error only currently
-    theta = init_theta # set a mutatable theta
+def gradient_descent(i, alpha, x, y): # squared error only currently
+    r"""
+    gradient_descent(i, alpha, x, y, init_theta)
+
+    Runs gradient descent on a specific x and y vector with i iterations and a learning rate, alpha.
+
+    Parameters
+    ----------
+    i : int
+        the number of times for gradient descent to run
+    alpha : float
+        The learning rate for gradient descent should be chosen such that it isn't too small or too large. If it is too small, gradient descent will be slow. If it is too large, gradient descent may overshoot and end with a bad error rate.
+    x : numpy.array
+        a 2d numpy array which describes the feature set
+    y : numpy.array
+        a 2d numpy array which holds the y values
+
+    Returns
+    -------
+    output : numpy.array
+        a 2d numpy array with the theta values optimized for the given x and y vectors
+    """
+
+    theta = generate(2) # set an initial theta vector
     m = x.shape[0] # number of rows in x
     X = np.hstack((np.ones((m,1)),x)) # x with a prepended column of ones
 
