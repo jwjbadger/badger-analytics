8dc2e921e4d8256914ea7909021cae573f52c639
jwjbadger <jwjbadger@gmail.com>
2021-06-08 16:45:07 -0500

    add documentation to data_import

--- banalytics/data_import.py
+++ banalytics/data_import.py
@@ -2,6 +2,21 @@ import numpy as np
 import requests
 
 def local_import(path):
+    """
+    local_import(path)
+
+    Import data from a file on your computer.
+
+    Parameters
+    ----------
+    path : string
+        the path to the file to open
+    
+    Returns
+    -------
+    output : numpy array
+        an array with the read data
+    """
     try:
         raw = open(path, 'r')
         return np.array(eval(raw.read()))
@@ -9,5 +24,20 @@ def local_import(path):
         raw.close()
 
 def remote_import(path):
+    """
+    remote_import(path)
+
+    Import a file from a URL.
+
+    Parameters
+    ----------
+    path : string
+        the URL to the file to get
+    
+    Returns
+    -------
+    output : numpy array
+        an array with the read data
+    """
     raw = requests.get(path)
     return np.array(eval(raw.text))
