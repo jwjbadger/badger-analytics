fix feature scaling and add tests for zscore

banalytics/processing/feature_scaling.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def standardization(input_set):
+def zscore(input):
     """
-    standardization(input_set)
+    zscore(input)
 
-    Standardizes the data stored within `input_set`.
+    Standardizes the data stored within `input`.
 
     Parameters
     ----------
-    input_set : numpy.array
+    input : numpy.array
         a numpy array to standardize
 
     Returns
@@ -17,21 +17,25 @@ def standardization(input_set):
         a numpy array with the standardized values of the original array
     """
 
-    result = np.array([])
-    X = np.hsplit(input_set, len(input_set[0])) # split the input array into columns
-    for i in X:
-        result = np.append(result, ((i - np.mean(i))/np.std(i)))
-    return result
+    if input.shape[1] == 1:
+        i = input.T
+        return np.round((i - np.mean(i))/np.std(i), 6).T
+    else:
+        result = np.hsplit(input, input.shape[1]) # Split the input into it's columns
+        for i in range(len(result)): # Iterate through each column
+            col = result[i] # Create a variable which stores the column
+            result[i] = (col - np.mean(col))/np.std(col) # Set the ith column in the result to the z score of the ith column
+        return np.round(np.hstack(result), 6) # Stack the columns properly
 
-def normalization(input_set):
+def normalize(input):
     """
-    normalization(input_set)
+    normalize(input)
 
-    Normalizes the data stored within `input_set`.
+    Normalizes the data stored within `input`.
 
     Parameters
     ----------
-    input_set : numpy.array
+    input : numpy.array
         a numpy array to normalize
 
     Returns
@@ -39,9 +43,12 @@ def normalization(input_set):
     output : numpy.array
         a numpy array with the normalized values of the original array
     """
-
-    result = np.array([])
-    X = np.hsplit(input_set, len(input_set[0])) # split the input array into columns
-    for i in X:
-        result = np.append(result, (i-min(i))/(max(i)-min(i)))
-    return result
+    if input.shape[1] == 1:
+        i = input.T
+        return np.round((i-min(i))/(max(i)-min(i)), 6).T
+    else:
+        result = np.hsplit(input, input.shape[1]) # Split the input into it's columns
+        for i in range(len(result)): # Iterate through each column
+            col = result[i] # Create a variable which stores the column
+            result[i] = (col-min(col))/(max(col)-min(col)) # Set the ith column in the result to the normalization of the ith column
+        return np.round(np.hstack(result), 6) # Stack the columns properly

banalytics/regression/linear.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # generates theta vector 
 def generate(n):
-    r"""
+    """
     generate(n)
 
     Generates a random theta vector of size n with a uniform distribution over [0, 10).
@@ -21,7 +21,7 @@ def generate(n):
     return np.multiply(np.random.rand(n, 1), 10)
 
 def squared_error(x, y, theta):
-    r"""
+    """
     squared_error(x, y, theta)
 
     Gives the squared error for given x and y vectors and a theta vector.
@@ -48,7 +48,7 @@ def squared_error(x, y, theta):
     return np.round((1/(2*m)) * np.sum(np.power((np.subtract(h, y)), 2)), 6)
 
 def gradient_descent(i, alpha, x, y): # squared error only currently
-    r"""
+    """
     gradient_descent(i, alpha, x, y, init_theta)
 
     Runs gradient descent on a specific x and y vector with i iterations and a learning rate, alpha.